9b92d92d8fdfc1bdca4753b12f6b20c61197334d
unknown <piyoshnikamble@gmail.com>
2022-08-17 14:40:13 +0530

    add changes

--- src/LogicalProgramme/Addition1.java
+++ src/LogicalProgramme/Addition1.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class Addition1 {
 
 	public static void main(String[] args) {
+		
+		//my changes
 		System.out.println("Enter the first number");
 Scanner sc=new Scanner(System.in);
 int n1 = sc.nextInt();
 System.out.println("Enter the second number");
 int n2=sc.nextInt();
+
+
 int sum=n1+n2;
 System.out.println("Addtion is "+sum);
 	}
--- src/LogicalStartpattern/SP6.java
+++ src/LogicalStartpattern/SP6.java
@@ -7,39 +7,49 @@ public class SP6 {
 //          ***
 //         *****
 //        *******
-	//	**********
+	//	 *********
+//        *******
 //         *****
 //          ***
-//           *
-		int space=4;
-		int star=1;
-		for(int i=1;i<=4;i++)
-		{
-			
-			for(int j=1;j<=space;j++)
-			{			
-			System.out.print(" ");
-			}
-			for(int k=1;k<=star;k++)
-			{
-				System.out.print("*");
-			}
-			space--;
-			star=star+2;
-			System.out.println();
-		}
-		for(int i=5;i>=1;i--)
-		{			
-		{
-			for(int j=1;j<=space;j++)
-			System.out.print(" ");
-		}
-	   for(int k=1;k<=star;k++)
-	   {
-		   System.out.print("*");
-	   }
-		
-		}
+//		     **
+		//   *
+		 int space=3;
+		   int star=1;
+		   for(int i=1;i<=4;i++)
+		   {
+			   for(int j=1;j<=space;j++)
+			   {
+				   
+				   System.out.print(" ");
+			   }
+			   for(int k=1;k<=star;k++)
+			   {
+				   System.out.print("*");
+			   }
+			   space--;
+			   star=star+2;
+			   System.out.println();
+			   
+		   }
+		 for(int i=4;i<=1;i++)
+		  {
+			 for(int j=1;j<=space;j++)
+			 {		 
+			 System.out.print(" "); 
+			 }
+			 for(int k=1;k<=star;k++)
+			 {
+				 System.out.print("*");
+			 }
+			 space++;
+			 star=star-2;
+		  }
+		 
 	}
-
 }
+	
+	
+	
+	
+	
+	
